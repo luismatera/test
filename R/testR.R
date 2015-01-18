df54de26e134d1d8baf2a5037fa10ab0aac09e9f
usuario <luis.matera@gmail.com>
2015-01-18 11:14:45 +0100

    first commit

--- R/testR.R
+++ R/testR.R
@@ -1,14 +1,15 @@
-#' Print hello
-#'
-#' This function prints hello
-#'
-#' @param fname First name
-#' @param lname Last name
-#' @export
-#' @examples
-#' hello(fname="Tomas",lname="Greif")
-#' hello(fname="Your",lname="Name")
-
-hello <- function(fname, lname) {
-  cat(paste("Hello",fname,lname,"!"))
+# Función chorrada 1
+f1 <- function(x, y) {
+  x+y
 }
+
+f1( 3, 4)
+
+# Función chorrada 2
+f2 <- function() {
+  print("Estamos a dia y son las:")
+  Sys.time()
+}
+
+f1(3, 4)
+f2()
